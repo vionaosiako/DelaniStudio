383b65a7c8e14a8abace196c6c99df1c5d952a2e
vionaosiako <viona.osiako@student.morigaschool.com>
2022-03-10 10:17:03 +0300

    Add hovering effects on the portfolio

--- js/script.js
+++ js/script.js
@@ -22,8 +22,58 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-  $("").hover(function(){
-    
-  })
+  $("#work3").hover(function(){
+    $("#work3Description").toggle();    
+  });
 
-})
+});
+
+$(document).ready(function(){
+  $("#work4").hover(function(){
+    $("#work4Description").toggle();    
+  });
+
+});
+
+$(document).ready(function(){
+  $("#work2").hover(function(){
+    $("#work2Description").toggle();    
+  });
+
+});
+
+$(document).ready(function(){
+  $("#work1").hover(function(){
+    $("#work1Description").toggle();    
+  });
+
+});
+
+$(document).ready(function(){
+  $("#work5").hover(function(){
+    $("#work5Description").toggle();    
+  });
+
+});
+
+$(document).ready(function(){
+  $("#work6").hover(function(){
+    $("#work6Description").toggle();    
+  });
+
+});
+
+$(document).ready(function(){
+  $("#work7").hover(function(){
+    $("#work7Description").toggle();    
+  });
+
+});
+
+$(document).ready(function(){
+  $("#work8").hover(function(){
+    $("#work8Description").toggle();    
+  });
+
+});
+
